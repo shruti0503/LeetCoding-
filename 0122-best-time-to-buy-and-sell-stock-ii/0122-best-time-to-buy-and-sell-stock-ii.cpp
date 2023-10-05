74a97e29bcffe5f83810feeaf97a102212ca02a6
Shruti <94033084+shruti0503@users.noreply.github.com>
2023-10-06 01:24:23 +0530

    Time: 14 ms (8.03%), Space: 17.5 MB (8.71%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,10 +21,32 @@ public:
         return dp[ind][flag]=maxprofit;
     }
 
+    int tab(vector<int>& prices) {
+    int n = prices.size();
+    vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+    for (int ind = n - 1; ind >= 0; --ind) {
+        for (int state = 0; state <= 1; ++state) {
+            if (state == 0) {
+                int buy = -prices[ind] + dp[ind + 1][1];
+                int skip = dp[ind + 1][0];
+                dp[ind][0] = max(buy, skip);
+            } else {
+                int sell = prices[ind] + dp[ind + 1][0];
+                int skip = dp[ind + 1][1];
+                dp[ind][1] = max(sell, skip);
+            }
+        }
+    }
+
+    return dp[0][0]; // Result for the original problem, starting in the buying state.
+}
+
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         vector<vector<int>>dp(n+1, vector<int>(2,-1));
 
-        return func(0, prices, false,dp); // Start with flag=false (buying state)
+       // return func(0, prices, false,dp); // Start with flag=false (buying state)
+       return tab(prices);
     }
 };
