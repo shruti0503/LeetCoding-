655af7c02b3066ff2b895fbf71ac80b32a6929ea
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-07-24 18:15:24 +0530

    Time: 29 ms (54.74%), Space: 16.3 MB (58.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,50 +1,31 @@
 class Solution {
 public:
+   void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& vis){
+     // checking for out of bbounds
+    if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' || vis[i][j]) {
+            return;
+     }
+     vis[i][j]=true;
+     dfs(grid, i-1, j, vis);
+     dfs(grid, i+1, j, vis);
+     dfs(grid, i, j - 1, vis); // Left
+     dfs(grid, i, j + 1, vis); // Right
 
-    void bfs(vector<vector<char>>& grid, int r, int c, vector<vector<bool>>& vis){
-
-        int rows=grid.size();
-        int cols=grid[0].size();
-
-        queue<pair<int,int>>pq;
-        pq.push({r,c});
-
-        vector<pair<int,int>> directions={{0,1},{1,0}, {0,-1}, {-1, 0}};
-
-        while(!pq.empty()){
-            auto [i,j]=pq.front();
-            pq.pop();
-
-            for(auto dir:directions){
-                // {0,1}
-                int ni=i+dir.first;
-                int nj=j+dir.second;
-
-                if(ni>=0 and ni<rows and nj>=0 and nj<cols and grid[i][j]=='1' and !vis[ni][nj]){
-                    pq.push({ni,nj});
-                    vis[ni][nj]=true;
-                }
-
-            }
-        }
-    
-    }
-
+   }
     int numIslands(vector<vector<char>>& grid) {
 
-        if(grid.empty()) return true;
         int res=0;
-        int rows=grid.size();
-        int cols=grid[0].size();
+        vector<vector<bool>> vis(grid.size(), vector<bool>(grid[0].size(), false));
 
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        //iterate over each cell in the grid
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1' and !vis[i][j]){
+                    res++; // count of island
+                    dfs(grid, i, j , vis);
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]=='1' and !visited[i][j]){
-                    res++;
-                    bfs(grid, i, j, visited);
                 }
+
             }
         }
 
