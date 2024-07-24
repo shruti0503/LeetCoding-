ecf07cbfa7057de3390bb0e33946851e13a6a76b
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-07-24 17:42:19 +0530

    Time: 28 ms (61.61%), Space: 24.2 MB (13.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,39 +1,54 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int res = 0;
-        // Create a visited matrix to track visited cells
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
 
-        // Iterate over each cell in the grid
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                // If the cell contains '1' and has not been visited before
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    // Increment the count of islands
+    void bfs(vector<vector<char>>& grid, int r, int c, vector<vector<bool>>& vis){
+
+        int rows=grid.size();
+        int cols=grid[0].size();
+
+        queue<pair<int,int>>pq;
+        pq.push({r,c});
+
+        vector<pair<int,int>> directions={{0,1},{1,0}, {0,-1}, {-1, 0}};
+
+        while(!pq.empty()){
+            auto [i,j]=pq.front();
+            pq.pop();
+
+            for(auto dir:directions){
+                // {0,1}
+                int ni=i+dir.first;
+                int nj=j+dir.second;
+
+                if(ni>=0 and ni<rows and nj>=0 and nj<cols and grid[i][j]=='1' and !vis[ni][nj]){
+                    pq.push({ni,nj});
+                    vis[ni][nj]=true;
+                }
+
+            }
+        }
+    
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+
+        if(grid.empty()) return true;
+        int res=0;
+        int rows=grid.size();
+        int cols=grid[0].size();
+
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1' and !visited[i][j]){
                     res++;
-                    // Call the dfs function to explore and mark the connected cells
-                    dfs(grid, i, j, visited);
+                    bfs(grid, i, j, visited);
                 }
             }
         }
 
         return res;
-    }
-
-    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited) {
-        // Base case: check for out of bounds or visited cells
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' || visited[i][j]) {
-            return;
-        }
-
-        // Mark the current cell as visited
-        visited[i][j] = true;
-
-        // Recursive calls to explore the adjacent cells
-        dfs(grid, i - 1, j, visited); // Up
-        dfs(grid, i + 1, j, visited); // Down
-        dfs(grid, i, j - 1, visited); // Left
-        dfs(grid, i, j + 1, visited); // Right
+        
     }
 };
