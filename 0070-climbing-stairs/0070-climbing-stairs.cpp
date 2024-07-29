b0dc7d9e7b8ccf61ff50a9a330cd0a78b1574ce4
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-07-29 23:28:20 +0530

    Time: 0 ms (100%), Space: 6.8 MB (100%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int step2before = 0, step1before = 1, cur = 0;
-        for (int i = 0; i < n; i++) {
-            cur = step2before + step1before;
-            step2before = step1before;
-            step1before = cur;
+    int climbStairs(int n, unordered_map<int, int>& memo) {
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        return cur;
-        
+        if (memo.find(n) == memo.end()) {
+            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+        }
+        return memo[n];
+    }
+
+    int climbStairs(int n) {
+        unordered_map<int, int> memo;
+        return climbStairs(n, memo);
     }
 };
