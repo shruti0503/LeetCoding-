37c06e91de8b20492593e7d8b3d46db1f94cd53a
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-08-02 01:40:20 +0530

    Time: 499 ms (22.62%), Space: 295 MB (5.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-  int func(vector<int>& nums, int ind, int prev, vector<vector<int>>& dp){
-      if(ind == nums.size()){
-          return 0;
-      }
-      if(dp[ind][prev+1] != -1){
-          return dp[ind][prev+1];
-      }
-      int take = 0;
-      // If the current ele is greater than prev, then count it in the LIS
-      if(prev == -1 || nums[ind] > nums[prev]){
-          take = 1 + func(nums, ind + 1, ind, dp);
-      }
-      // Do not pick the element, keep the previous element
-      int nottake = func(nums, ind + 1, prev, dp);
-      int ans = max(take, nottake);
-      dp[ind][prev+1] = ans; // Store the result in dp
-      return ans;
-  }
+    int func(vector<int>& nums, int ind, int prev, int n, vector<vector<int>>& dp) {
+        // Base case: If we've considered all elements
+        if (ind == n) return 0;
+        
+        // Check if the result is already computed
+        if (dp[ind][prev + 1] != -1) return dp[ind][prev + 1];
+        
+        // Not taking the current element
+        int notTake = func(nums, ind + 1, prev, n, dp);
+        
+        // Taking the current element (if it forms an increasing subsequence)
+        int take = 0;
+        if (prev == -1 || nums[ind] > nums[prev]) {
+            take = 1 + func(nums, ind + 1, ind, n, dp);
+        }
+        
+        // Store the result in dp array
+        return dp[ind][prev + 1] = max(take, notTake);
+    }
 
-  int lengthOfLIS(vector<int>& nums) {
-      int n = nums.size();
-      vector<vector<int>> dp(n+1, vector<int>(n+1, -1)); // Correct allocation
-      return func(nums, 0, -1, dp);
-  }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return func(nums, 0, -1, n, dp);
+    }
 };
