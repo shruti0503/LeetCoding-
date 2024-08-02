0ceee627f00c043fe2a368d12e2b374ba6de69c4
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-08-02 15:59:54 +0530

    Time: 244 ms (34.84%), Space: 574.7 MB (5.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -20,9 +20,27 @@ public:
         return dp[ind][prev + 1] = max(take, notTake);
     }
 
+   int tab(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prev = ind - 1; prev >= -1; prev--) {
+                int notTake = dp[ind + 1][prev + 1];
+                int take = 0;
+                if (prev == -1 || nums[ind] > nums[prev]) {
+                    take = 1 + dp[ind + 1][ind + 1];
+                }
+                dp[ind][prev + 1] = max(take, notTake);
+            }
+        }
+        return dp[0][-1 + 1];
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return func(nums, 0, -1, n, dp);
+        //return func(nums, 0, -1, n, dp);
+        return tab(nums);
     }
 };
