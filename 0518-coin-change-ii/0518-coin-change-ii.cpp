36d04d52b9ec9c974133cca5d54d37ebc7a2ed32
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-08-05 21:59:37 +0530

    Time: 28 ms (10.68%), Space: 20.2 MB (31.95%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int rec(int amount, int ind, vector<int>& coins, vector<vector<int>>& dp) {
+   int func(int amount, vector<int>& coins, int ind, int ans,vector<vector<int>>& dp){
+        
         if (amount == 0) {
             return 1;
         }
         if (amount < 0 || ind >= coins.size()) {
             return 0;
         }
-        if (dp[ind][amount] != -1) {
-            return dp[ind][amount];
-        }
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
 
-        // Include the current coin and move to the next index
-        int includeCurrent = rec(amount - coins[ind], ind, coins, dp);
+       
 
-        // Exclude the current coin and move to the next index
-        int excludeCurrent = rec(amount, ind + 1, coins, dp);
 
-        return dp[ind][amount]=includeCurrent+ excludeCurrent;
-    }
+    //    for(int i=ind;i<coins.size();i++){
+        // take 
+         int take =func(amount-coins[ind], coins, ind, ans,dp);
 
+         // not take
+         int notTake=func(amount, coins, ind+1, ans,dp);
+    //    }
+
+       return dp[ind][amount]= take+notTake;
+
+
+    
+
+   }
     int change(int amount, vector<int>& coins) {
-        int n= coins.size();
-         vector<vector < int>> dp(n + 1, vector<int> (amount + 1, -1));
-        return rec(amount, 0, coins, dp);
+        int n=coins.size();
+        //If you have an amount of 5, you need to consider indices from 0 to 5, which totals 6 indices. Thus, the array needs to be of size amount + 1.
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        return func(amount, coins, 0, 0,dp);
+        
     }
 };
