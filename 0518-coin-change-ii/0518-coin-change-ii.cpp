e00e3851ca832008b128283673621833194b1821
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-08-05 22:44:23 +0530

    Time: 15 ms (36.37%), Space: 20.4 MB (17.3%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -41,9 +41,6 @@ public:
      }
 
     return dp[0][money];
-
-      
-
    }
 
    int tab2(int money, vector<int>& coins){
@@ -68,14 +65,45 @@ public:
 
         return dp[0][money];
    }
+   int spaceOpt(int money, vector<int>& coins){
+
+    int n=coins.size();
+    // vector<vector<int>> dp(n+1, vector<int>(money+1, 0));
+     vector<int> curr (money+1,0);
+     vector<int>next(money+1,0);
+      // Base case: If amount is 0, there's one way to make the amount (use no coins)
+    //    for (int i = 0; i <= n; ++i) {
+    //     [i][0] = 1;
+    // }
+    curr[0]=1;
+    next[0]=1;
+
+     for(int ind=n-1;ind>=0;ind--){
+        for(int amount=0;amount<=money;amount++){  
+        int take=0; // [amount-coins[ind]]= can result in negative index -> can give runtime error
+        if(amount>=coins[ind]){
+             take = curr[amount - coins[ind]];
+        } 
+
+         int notTake=next[amount];
+          curr[amount]=take+notTake;
+
+        }
+       next=curr; // Move current row to next row for next iteration
+     }
+
+    return curr[money];
+
+   }
 
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         //If you have an amount of 5, you need to consider indices from 0 to 5, which totals 6 indices. Thus, the array needs to be of size amount + 1.
         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
        // return func(amount, coins, 0, 0,dp);
-        return tab(amount, coins);
-        return tab2(amount,coins);
+        // return tab(amount, coins);
+        // return tab2(amount,coins);
+        return spaceOpt(amount, coins);
         
     }
 };
