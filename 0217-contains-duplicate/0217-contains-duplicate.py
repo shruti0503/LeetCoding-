4b99c61136cd4fe4266a40834dc3ff3cfd3be700
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-08-12 00:53:58 +0530

    Time: 490 ms (7.51%), Space: 21.5 MB (92.27%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(1, len(nums)):
+            if(nums[i]==nums[i-1]):
+                 return True
+        return False
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
