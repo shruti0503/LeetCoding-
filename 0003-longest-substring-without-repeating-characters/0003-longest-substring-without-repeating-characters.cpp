11616e1274bf94b1dcfaeb1dfaee66ae54137be2
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-08-22 01:49:26 +0530

    Time: 30 ms (14.96%), Space: 14.7 MB (17.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> ldss(256,0); // ascii code of a-z,  space, A-Z, digits fall under the number range of 0 - 255
-        int i = 0, n = s.size(), j = 0;
-        int ans = 0;
-        while(i < n)
-        {
-            if(ldss[s[i]] == 1)
-            {
-                ldss[s[j]]--;
+        unordered_set<char>letters ;
+        int i=0;
+        int j=0;
+        int result=0;
+        while(j<s.size()){
+            if(letters.find(s[j])==letters.end()){
+                letters.insert(s[j]);
+                result=max(result,j-i+1);
                 j++;
             }
-            else
-            {
-                ldss[s[i]]++;
-                ans = max(ans, i-j+1);
+            else{
+                letters.erase(s[i]);
                 i++;
             }
         }
-        return ans;
+
+        return result;
+
+        
     }
 };
