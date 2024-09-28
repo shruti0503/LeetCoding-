39016537b83f35ea12509df4abbe51183df1e8cb
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-09-29 00:25:26 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.14%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    private:
-    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
-        //base case
-        if(index>=nums.size()) {
-            ans.push_back(output);
-            return ;
-        }
-        
-        //exclude
-        solve(nums, output, index+1, ans);
-        
-        //include
-        int element = nums[index];
-        output.push_back(element);
-        solve(nums, output, index+1, ans);
-        
-    }
 public:
+  void func(vector<int>& nums, int ind, vector<vector<int>>& ans, vector<int>& res ){
+
+    if(ind>=nums.size()){
+        ans.push_back(res) ;
+        return;
+
+    } 
+
+    // include
+    res.push_back(nums[ind]);
+    func(nums, ind+1, ans,res);
+    res.pop_back();
+
+    //exclude 
+    func(nums, ind+1, ans, res);
+    
+  }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int> > ans;
-        vector<int> output;
-        int index = 0;
-        solve(nums, output, index, ans);
+        vector<vector<int>> ans;
+        vector<int>res;
+        func(nums, 0, ans,res);
         return ans;
+        
     }
 };
