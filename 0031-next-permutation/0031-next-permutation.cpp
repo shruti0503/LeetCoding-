07ba35e4f10209ae228ad1255e4a8c7e121629af
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-10-20 23:48:12 +0530

    Time: 0 ms (100%), Space: 14.9 MB (49.27%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int breakInd = -1;
-        int n = nums.size();
-
-        // Find the index where the current permutation breaks the increasing order
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] < nums[i + 1]) {
-                breakInd = i;
-            }
+    int n=nums.size();
+    int breakInd=-1;
+    for(int i=0;i<n-1;i++){
+        if(nums[i]<nums[i+1]){
+            breakInd=i;
         }
+    }
 
-        // If no such breakpoint exists, reverse the entire array to get the smallest permutation
-        if (breakInd == -1) {
-            reverse(nums.begin(), nums.end());
-            return;
+    // if no such breakpoint exits
+    if(breakInd==-1){
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+    // finf the smallest number to the right of the  breaking poiny
+    for(int i=n-1;i>breakInd;i--){
+        if(nums[i]>nums[breakInd]){
+            swap(nums[i], nums[breakInd]);
+            break;
         }
+    }
 
-        // Find the smallest number to the right of breakInd that is greater than nums[breakInd]
-        for (int i = n - 1; i > breakInd; i--) {
-            if (nums[i] > nums[breakInd]) {
-                swap(nums[i], nums[breakInd]);
-                break;
-            }
-        }
-
-        // Reverse the elements to the right of breakInd to get the next permutation
-        reverse(nums.begin() + breakInd + 1, nums.end());
+    // reverse the lemnts to the break ppint
+    reverse(nums.begin()+breakInd+1,nums.end());
     }
 };
