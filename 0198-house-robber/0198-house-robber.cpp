9fe19bb7d54b9bd2843b0f1df9a22ef792e5e546
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-06 02:33:23 +0530

    Time: 0 ms (100%), Space: 9.6 MB (45.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -35,11 +35,39 @@ public:
     return dp[0];
 
  }
+
+ int spaceOpt(vector<int>& nums){
+    int n=nums.size();
+    if (n == 0) return 0;
+    //vector<int>dp(n+1, 0);
+    //dp[n]=0;
+    int prev1=0;
+    int prev2=0;
+    int current;
+    for(int i=n-1;i>=0;i--){
+        int take;
+        if(i+2<=n){
+              take=nums[i]+prev2;
+        }
+        else{
+            take=nums[i];
+        }
+       
+        int notTake=prev1;
+        current=max(take, notTake);
+        prev2=prev1;
+        prev1=current;
+    }
+
+    return prev1;
+
+ }
    
     int rob(vector<int>& nums) {
         vector<int>dp(nums.size()+1,-1);
         //return func( 0,  nums,  dp);
-        return tab(nums);
+       // return tab(nums);
+       return spaceOpt(nums);
 
         
     }
