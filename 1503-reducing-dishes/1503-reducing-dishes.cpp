43cf20b084c688da47607a35016b04534dabda49
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-06 23:47:26 +0530

    Time: 19 ms (36.91%), Space: 35.9 MB (8.85%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+// Maximizing Satisfaction: The goal is to maximize the sum of satisfactions, 
+// calculated as satisfaction[i] * (time + 1) for each dish i. 
+// If you choose to serve a dish with high satisfaction earlier,
+//  the multiplier (time + 1) will be smaller than if you served it later.
+//   By sorting the array in ascending order, you ensure that lower satisfaction values 
+//   are handled first,
+//  leaving space for larger values to have a bigger impact.
    int func(vector<int>& sat, int time , int ind, vector<vector<int>>& dp){
         if(ind>=sat.size()){
             return 0;
@@ -10,15 +17,31 @@ public:
 
          dp[ind][time]=max(include, exclude);
          return dp[ind][time];
-
-         
-
    }
+    int tab(vector<int>& sat){
+        int n=sat.size();
+       sort(sat.begin(), sat.end());
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0)); // yhi ke yhi intialise kr do
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int time=ind; time>=0;time--){
+                int include=(sat[ind]*(time+1))+dp[ind+1][time+1];
+                int exclude=dp[ind+1][time];
+                dp[ind][time]=max(include, exclude);
+                
+            }
+        }
+
+        return dp[0][0];
+       
+
+    }
     int maxSatisfaction(vector<int>& satisfaction) {
         int n=satisfaction.size();
         sort(satisfaction.begin(), satisfaction.end());
         vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return func(satisfaction, 1, 0 , dp);
+        //return func(satisfaction, 1, 0 , dp);
+        return tab(satisfaction);
         
     }
 };
