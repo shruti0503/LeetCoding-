891a0bffae83bf5138a89ae78aac379933ddae9b
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-09 00:02:40 +0530

    Time: 0 ms (100%), Space: 18.3 MB (83.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-  void  dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis){
-    vis[node]=1;
-    for(int i=0;i<isConnected[node].size();i++){
+  void func(int node,vector<vector<int>>&isConnected, vector<int>& vis ){
+     vis[node]=1;
+     // travesers ein the adj c list
+     for(int i=0;i<isConnected.size();i++){
+        // check if edge exists and is not  marked as vsiisted
         if(isConnected[node][i] and !vis[i]){
-            dfs(i, isConnected,vis);
+            func(i, isConnected, vis);
         }
-    }
+     }
   }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        // isConncetd is the graph , that shows connectoon
-        // and isCOnnected.size() is the numbver of vertices we have
         int n=isConnected.size();
         int m=isConnected.size();
         vector<int>vis(n,0);
         int cnt=0;
+       //  dfs on  each and every node
 
         for(int i=0;i<isConnected.size();i++){
             if(vis[i]!=1){
-                dfs(i,isConnected, vis);
+                func(i, isConnected, vis);
                 cnt++;
-
             }
         }
 
         return cnt;
+        
     }
 };
