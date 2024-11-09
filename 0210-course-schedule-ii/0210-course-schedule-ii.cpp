7dcdde44319347746fddf814309f84bcfcd9bba6
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-09 16:21:53 +0530

    Time: 0 ms (100%), Space: 17.6 MB (61.06%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,54 +1,55 @@
 class Solution {
 public:
-// First, we will form the adjacency list of the graph using the pairs. For example, for the pair {u, v} we will add node v as an adjacent node of u in the list.
-    vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        //creating adj c list;
-        vector<int> adj[n];
-        //add the index of the prewq course to th list of incoming edges for the dependnt source in the adj list.
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& inStack, stack<int>& st) {
+        // If the node is already in the current recursion stack, it's a cycle
+        if (inStack[node]) return false;
+        
+        // If it's already visited, no need to explore again
+        if (vis[node]) return true;
 
-        for(auto it:pre){
-            adj[it[1]].push_back(it[0]);
-            //adj[i[i]] or  adj[it.first] = accessing list of seconf elemnet
-            //adj[i[1]].push_back(i[0]);
-            // it.first =access the second elemnt of of 'it', which represents course that depends on the prereq.
-        }
-        vector<int>topo;
-        //calculate the indegree
-        //iterarte through the givem adj list and simply for every node u->v, we can increse the indegree v by 1 in the indgree arr
-        vector<int> indegree(n,0);
-        for(int node=0;node<n;node++){
-            for(auto j:adj[node]){
-                indegree[j]++;
+        // Mark the node as visited and add to the recursion stack
+        vis[node] = 1;
+        inStack[node] = 1;
+
+        // Explore all the adjacent nodes
+        for (auto it : adj[node]) {
+            if (!dfs(it, adj, vis, inStack, st)) {
+                return false;
             }
         }
-    //initally there will alwasys be  at least a single node whose indegree is 0.
-    //so push it in queue.
-    queue<int>q;
-        for(int node=0;node<n;node++){
-            if(!indegree[node]){
-                q.push(node);
+
+        // After finishing this node, push it to the stack and remove from the recursion stack
+        inStack[node] = 0;
+        st.push(node);
+        return true;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses); // adjacency list
+        vector<int> vis(numCourses, 0);  // to mark nodes as visited
+        vector<int> inStack(numCourses, 0);  // to mark nodes in the current DFS path
+        stack<int> st;  // stack to store the topological order
+        
+        // Build the graph (adjacency list)
+        for (auto& pre : prerequisites) {
+            adj[pre[1]].push_back(pre[0]);
+        }
+
+        // Try to find a valid topological sort using DFS
+        for (int i = 0; i < numCourses; ++i) {
+            if (!vis[i]) {
+                if (!dfs(i, adj, vis, inStack, st)) {
+                    return {};  // If there's a cycle, return an empty array
+                }
             }
         }
-    //pop a node from the queue includng the node in our own topo aarray and for all the adjcant nodes, decrese the indegree of that node by one.
 
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto it:adj[node]){
-                indegree[it]--;
-                 if(!indegree[it]) q.push(it);
-                
-            }
-            //if for any node the indegree becomes zero;push that node again in queue.
-           
+        // Collect the result from the stack
+        vector<int> result;
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
         }
-
-        if(topo.size()!=n){
-            return {};
-        }
-
-        return topo;
-
+        return result;
     }
 };
