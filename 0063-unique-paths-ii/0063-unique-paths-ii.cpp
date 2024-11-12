4e76cb8f1250e0e2c279c923d5b542e637eb2b0f
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-12 23:10:09 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -40,6 +40,34 @@ public:
 
     }
 
+    int spaceOpt(vector<vector<int>>& obstacleGrid){
+         int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+
+        // Create a 1D dp array for the current row
+        vector<int> dp(m, 0);
+        
+        // Initialize the starting point
+        dp[0] = (obstacleGrid[0][0] == 0) ? 1 : 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    // Set dp[j] to 0 if there's an obstacle
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    // Add the value from the left cell in the same row
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+
+        // The answer is in dp[m-1], the last cell of the 1D array
+        return dp[m - 1];
+
+    }
+
+
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         // two paramters are changing
         int ways;
@@ -50,8 +78,8 @@ public:
 
        //return func(n, m, obstacleGrid,0,0,ways,dp);
        
-       return tab(obstacleGrid);
-        
+       //return tab(obstacleGrid);
+        return spaceOpt(obstacleGrid);
     }
         
     
