d7786a03866fc84101ffa9bb6e503f8a2abed174
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-12 22:06:17 +0530

    Time: 0 ms (100%), Space: 8.1 MB (20.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int cnt(int right, int down, int m, int n, vector<vector<int>>& dp) {
-        if (right > n - 1 || down > m - 1) {
-            return 0;
-        }
-        if (right == n - 1 && down == m - 1) {
-            return 1;
-        }
+   int func(int right, int down, int m , int n,vector<vector<int>>& dp){
+            if(right>n-1 || down>m-1){
+                return 0;
+            }
+            if(right==n-1 and down==m-1){
+                return 1;
+            }
+            if(dp[down][right]!=-1){
+                return dp[down][right];
+            }
 
-        if (dp[down][right] != -1) {
-            return dp[down][right];
-        }
-
-        return dp[down][right] = cnt(right + 1, down, m, n, dp) + cnt(right, down + 1, m, n, dp);
-    }
-
+        return dp[down][right]=func(right+1, down , m, n, dp)+func(right, down+1, m, n,dp);
+   }
+   
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return cnt(0, 0, m, n, dp);
+
+         vector<vector<int>> dp(m, vector<int>(n, -1));
+        return func(0, 0, m, n, dp);
+        
     }
 };
