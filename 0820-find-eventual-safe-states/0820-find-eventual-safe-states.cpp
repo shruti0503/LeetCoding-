fc0baff50dcee38de7b82477d4c8ab42f53ea500
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-16 13:08:06 +0530

    Time: 4 ms (93.79%), Space: 52.2 MB (76.11%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    // Helper function to perform DFS
-        bool dfs(int node, vector<vector<int>>& graph, vector<int>& states) {
-            if (states[node] == 1) {  // If the node is currently being visited, it means we've found a cycle.
-                return false;
+    bool dfs(vector<vector<int>>& graph, int node, vector<int>& pathVis, vector<int>& vis, vector<int>& safeNode){
+         vis[node]=1;
+          pathVis[node] = 1; 
+
+         for(auto adj:graph[node]){
+            if(!vis[adj]){
+               if( dfs(graph, adj, pathVis,vis , safeNode)){
+                 safeNode[node]=-1; //The purpose of safeNodes[node] = -1 is to indicate that node itself is unsafe because it leads to a cycle either directly or indirectly through one of its adjacent nodes (adj). Since node is calling the DFS on adj, any cycle detected in adj means that node is also part of an unsafe path.
+                 return true;
+               } 
             }
-            if (states[node] == 2) {  // If the node is already safe, no need to visit again.
+            else if(pathVis[adj]){
+                safeNode[node]=-1;
                 return true;
             }
-            
-            states[node] = 1;  // Mark as visiting
-            
-            // Explore all adjacent nodes
-            for (int neighbor : graph[node]) {
-                if (!dfs(neighbor, graph, states)) {
-                    return false;
-                }
-            }
-            
-            states[node] = 2;  // Mark as safe
-            return true;
-        }
+         }
+
+         pathVis[node]=0;
+         safeNode[node]=1;
+         return false;
+    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> states(n, 0);  // States: 0 = unvisited, 1 = visiting, 2 = safe
-        vector<int> safeNodes;
-        
-        // Perform DFS for all nodes
-        for (int i = 0; i < n; i++) {
-            if (states[i] == 0) {  // If the node is unvisited
-                dfs(i, graph, states);
+        int n=graph.size();
+        vector<int>vis(n,0);
+        vector<int>pathVis(n,0);
+        vector<int> safeNodes(n,0);
+
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(graph, i, pathVis, vis,safeNodes);
             }
         }
-        
-        // Collect safe nodes
-        for (int i = 0; i < n; i++) {
-            if (states[i] == 2) {
-                safeNodes.push_back(i);
+
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(safeNodes[i]==1){
+                ans.push_back(i);
             }
         }
+        return ans;
         
-        return safeNodes;
     }
 };
