9b1e9ed0b16423c35b483fea86f3621639d06f2a
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-16 13:51:52 +0530

    Time: 5 ms (50.37%), Space: 17.9 MB (42.48%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& inStack, stack<int>& st) {
-        // If the node is already in the current recursion stack, it's a cycle
-        if (inStack[node]) return false;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+
+        //1.creatiung adj c list 
+        vector<vector<int>>adj(numCourses);
+        vector<int>indeg(numCourses,0);
+        for(auto course:prerequisites){
+            adj[course[1]].push_back(course[0]);
+            indeg[course[0]]++;
+        }
         
-        // If it's already visited, no need to explore again
-        if (vis[node]) return true;
+        queue<int>pq;
+        vector<int>topo;
 
-        // Mark the node as visited and add to the recursion stack
-        vis[node] = 1;
-        inStack[node] = 1;
-
-        // Explore all the adjacent nodes
-        for (auto it : adj[node]) {
-            if (!dfs(it, adj, vis, inStack, st)) {
-                return false;
+        //3. while loop push in q
+        for(int i=0;i<indeg.size();i++){
+            if(indeg[i]==0){
+                pq.push(i);
             }
         }
 
-        // After finishing this node, push it to the stack and remove from the recursion stack
-        inStack[node] = 0;
-        st.push(node);
-        return true;
-    }
-
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses); // adjacency list
-        vector<int> vis(numCourses, 0);  // to mark nodes as visited
-        vector<int> inStack(numCourses, 0);  // to mark nodes in the current DFS path
-        stack<int> st;  // stack to store the topological order
-        
-        // Build the graph (adjacency list)
-        for (auto& pre : prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
-        }
-
-        // Try to find a valid topological sort using DFS
-        for (int i = 0; i < numCourses; ++i) {
-            if (!vis[i]) {
-                if (!dfs(i, adj, vis, inStack, st)) {
-                    return {};  // If there's a cycle, return an empty array
+        while(!pq.empty()){
+            int course=pq.front();
+            pq.pop();
+            topo.push_back(course);
+            for(auto adjNode:adj[course]){
+                indeg[adjNode]--;
+                if(indeg[adjNode]==0){
+                    pq.push(adjNode);
                 }
             }
+
+
         }
 
-        // Collect the result from the stack
-        vector<int> result;
-        while (!st.empty()) {
-            result.push_back(st.top());
-            st.pop();
+         if (topo.size() == numCourses) {
+            return topo;
         }
-        return result;
+         return {};
+        
+
     }
 };
