5613a5591d7e9f6b22f94f39a22b3c426c06792d
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-16 17:33:58 +0530

    Time: 0 ms (100%), Space: 11.4 MB (86.1%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-
+   // try to visulaise a mountian 
+ //             //  -> this is the peak elemnt
+ //           //  \\
+//           //    \\
+// if mid lies inthe left part we need to shift it to the peak 
+// therefore if(nums[mid]<=nums[mid+1]) // lies in left => move forward to the peak elemt
+// else it lies in right part so simply e=mid ; beacuse mid can also be a peak element
         int mid;
         int s=0;
         int e=nums.size()-1;
