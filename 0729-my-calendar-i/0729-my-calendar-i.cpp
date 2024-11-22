b772a34895229c37d4147f878da0441251875a06
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-23 01:32:38 +0530

    Time: 27 ms (39.49%), Space: 42.6 MB (49.78%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -6,6 +6,11 @@ public:
     MyCalendar() {
     }
 
+ //
+ //For each book call, you iterate through the list of bookings to check 
+ //for overlaps.
+ //  If there are  m callsto the book , the worst case time complexity is O(m2).
+ 
     bool book(int start, int end) {
         for (const pair<int, int>& event : bookings) {
             if (start < event.second && end > event.first) {
