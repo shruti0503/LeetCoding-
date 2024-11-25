5b159b71674413d79a9a33bbeddf3d7beb9c07cd
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-11-26 00:16:53 +0530

    Time: 5 ms (66.18%), Space: 19.9 MB (56.1%) - LeetHub

--- 1493-frog-position-after-t-seconds/1493-frog-position-after-t-seconds.cpp
+++ 1493-frog-position-after-t-seconds/1493-frog-position-after-t-seconds.cpp
@@ -1,52 +1,57 @@
 class Solution {
 public:
+    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+        vector<vector<int>>graph(n+1);
+        for(auto& edge:edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        //  for(auto& edge:edges){
+        //     // cout<<"adj nodes for "<<edge
+        //     for(auto it:edge){
+        //         cout<<"graph is "<<it<< endl;
+
+        //     }
+            
+        // }
+       
+        queue<pair<int, pair<double, int>>>q; // node , [probab, time]
+        q.push({1,{1.0,0}});
     
-double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
-    // Build the adjacency list
-    vector<vector<int>> graph(n + 1);
-    for (auto& edge : edges) {
-        graph[edge[0]].push_back(edge[1]);
-        graph[edge[1]].push_back(edge[0]);
+        vector<bool>vis(n+1, false);
+        vis[1]=true;
+
+        while(!q.empty()){
+            auto [node, probab_time]=q.front();
+            auto [probab, time]=probab_time;
+            q.pop();
+            if(time>t){
+                cout<<"stopping traversal as time exceeded, curr time is : "<<time<<endl;
+                continue;
+            }
+            if(node==target){ // If at target node and time == t or no more neighbors to jump
+                cout<<"node==target so , time is "<< time << " and graph[node].size() is  "<<graph[node].size()<<endl;
+                if(time==t || graph[node].size()==(node==1?0:1)){
+                    return probab;
+                }
+            }
+            int unvisited=0;
+            for(int adj:graph[node]){
+                if(!vis[adj]){
+                    unvisited++;
+                }
+            }
+
+            for (int neighbor : graph[node]) {
+            if (!vis[neighbor]) {
+                vis[neighbor] = true;
+                q.push({neighbor, {probab / unvisited, time + 1}});
+            }
+          }
+        }
+
+        return 0.0;
+
+        
     }
-
-    // BFS queue: {current node, probability, time elapsed}
-    queue<pair<int, pair<double, int>>> q;
-    q.push({1, {1.0, 0}});
-    vector<bool> visited(n + 1, false);
-    visited[1] = true;
-
-    while (!q.empty()) {
-        auto [node, prob_time] = q.front();
-        auto [prob, time] = prob_time;
-        q.pop();
-
-        // Stop traversal if time exceeds t
-        if (time > t) continue;
-
-        // If at target node and time == t or no more neighbors to jump
-        if (node == target) {
-            if (time == t || graph[node].size() == (node == 1 ? 0 : 1)) {
-                return prob;
-            }
-        }
-
-        // Calculate unvisited neighbors
-        int unvisited_count = 0;
-        for (int neighbor : graph[node]) {
-            if (!visited[neighbor]) {
-                unvisited_count++;
-            }
-        }
-
-        // Add unvisited neighbors to the queue
-        for (int neighbor : graph[node]) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
-                q.push({neighbor, {prob / unvisited_count, time + 1}});
-            }
-        }
-    }
-
-    return 0.0; // Target not reachable within t seconds
-}
 };
