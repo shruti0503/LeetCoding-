1b6ed182511fa8eb68cbf166029d76b5ac990bdb
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-12-08 13:33:37 +0530

    Time: 0 ms (100%), Space: 21.8 MB (8.66%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        // can insert an interval only when 
-        //it[1]>new[0];
-        // temp [1,2] 
-        // [[1,2] [3,8]  ]
-        // temp=[3,8]
-         vector<vector<int>>ans;
-        if(intervals.empty()) return{newInterval};
-        intervals.push_back(newInterval);
-         //[[1,3],[2,6],[8,10],[15,18]]
-         // temp=[1,3]
-         //[1,3]  temp[1]>=it[0] ->  temp[1]-> max of both -> temp=[1,6]
-         // [8,10]
-         // ans -> [[1,6] ,    ]
-         // temp= [8,10] it=[15,18];  15>10 so cannot merge -> temp=[15,18]
-         // [[1,6] , [8,10]]
-
-         sort(intervals.begin(), intervals.end());
-         vector<int> temp=intervals[0];
-         for(auto it:intervals){ // [1,3]
-           cout<<"interval  start it "<< it[0]<<endl;
-            if(it[0]<=temp[1]){
-                cout<<"end interval " << temp[1]<<endl;
-                temp[1]=max(it[1], temp[1]);
-                temp[0]=min(it[0], temp[0]);
-                cout<<"merge it "<< temp[0]<<" "<<temp[1]<<endl<<endl;
-            }
-            else{
-                ans.push_back(temp);
-                temp=it;
-            }
-         }
-         ans.push_back(temp);
-         return ans;
-         
-         // newInterval=[2,5]
-        //  for(it:intervals){
-    
-        //  }
+        vector<vector<int>>ans;
+        int n=intervals.size();
+        int i=0;
+        //intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+        while(i<n and intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]); 
+            i++;
+        }
+        //[[1,2]]
+        //now merge the overlapping 
+        while(i<n and intervals[i][0]<=newInterval[1]){
+            newInterval[0]=min(newInterval[0], intervals[i][0]);
+            newInterval[1]=max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+        while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
     }
 };
