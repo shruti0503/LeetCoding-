731c2aa4abf8295756e0df806724d439f0720a4e
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-12-08 13:04:27 +0530

    Time: 192 ms (5.05%), Space: 24.8 MB (9.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,6 +6,14 @@ public:
          if(intervals.size()==0){
             return ans;
          }
+         //[[1,3],[2,6],[8,10],[15,18]]
+         // temp=[1,3]
+         //[1,3]  temp[1]>=it[0] ->  temp[1]-> max of both -> temp=[1,6]
+         // [8,10]
+         // ans -> [[1,6] ,    ]
+         // temp= [8,10] it=[15,18];  15>10 so cannot merge -> temp=[15,18]
+         // [[1,6] , [8,10]]
+
          sort(intervals.begin(), intervals.end());
          vector<int> temp=intervals[0];
          for(auto it:intervals){ // [1,3]
