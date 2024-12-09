d29350858288120e313c8f7de77214abe658ae10
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-12-09 23:00:43 +0530

    Time: 106 ms (86.82%), Space: 121.6 MB (52.56%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,48 +1,43 @@
 #include <deque>
 
-
 class Solution {
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX)); 
-        deque<pair<int, int>> dq; 
+        int n=grid.size();
+        int m=grid[0].size();
 
-       
-        dist[0][0] = 0;
-        dq.push_front({0, 0});
+        vector<vector<int>>dist(n, vector<int>(m,INT_MAX));
+        deque<pair<int,int>>dq;
 
-       
-        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        dist[0][0]=0;
+        dq.push_front({0,0});
+        vector<pair<int,int>>directions={{0,1}, {1,0},{0,-1},{-1,0}};
 
-        while (!dq.empty()) {
-            auto [row, col] = dq.front();
+        while(!dq.empty()){
+            auto [row, col]=dq.front();
             dq.pop_front();
 
-            for (auto dir : directions) {
-                int nrow = row + dir.first;
-                int ncol = col + dir.second;
+            for(auto dir:directions){
+                int nrow=row+dir.first;
+                int ncol=col+dir.second;
 
-                // Check bounds
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
-                    int newDist = dist[row][col] + grid[nrow][ncol];
-                    if (newDist < dist[nrow][ncol]) { 
-                        dist[nrow][ncol] = newDist;
-
-                        // Push to front if no obstacle; to back otherwise
-                        if (grid[nrow][ncol] == 0) {
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m){
+                    int newDist=dist[row][col]+grid[nrow][ncol];
+                     if(newDist<dist[nrow][ncol]){
+                        dist[nrow][ncol]=newDist;
+                        //push to front if no obstacles ; to back otherwiose
+                        if(grid[nrow][ncol]==0){
                             dq.push_front({nrow, ncol});
-                        } else {
+                        }
+                        else{
                             dq.push_back({nrow, ncol});
                         }
-                    }
+                     }
                 }
             }
         }
 
-       
-        return dist[n - 1][m - 1];
+        return dist[n-1][m-1];
+        
     }
 };
