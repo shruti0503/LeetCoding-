2f8a87bfa6c788d96e36f56ed56514556d374959
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-12-17 00:37:40 +0530

    Time: 104 ms (14.11%), Space: 48.1 MB (15.46%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // initliase adjaceny lkist
-        vector<vector<int>>adj[n+1];
-        for(const auto& time: times){
-            int src=time[0];
-            int target=time[1];
-            int weight=time[2];
-            adj[src].push_back({target, weight});
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto it : times) {
+            adj[it[0]].push_back({it[1], it[2]});
         }
-        // min heap
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
 
-        vector<int> dist(n+1, INT_MAX);
-        dist[k]={0};
-        pq.push({k,0});
-        while(!pq.empty()){
-            int currDist=pq.top().second;
-            int node=pq.top().first;
+      
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, k}); // (time, node)
+
+        vector<int> dist(n + 1, INT_MAX); 
+        dist[k] = 0;
+
+        while (!pq.empty()) {
+            auto curr = pq.top();
             pq.pop();
 
-            // if the distance in the queue is greater than the current known distance,
-            // skip processing ; coz no point if the currDist is already grrater that mneans its already gonna reach this one too
-            if(currDist>dist[node]) continue;
+            int currTime = curr.first;
+            int node = curr.second;
 
-            for(const auto&  adjs: adj[node]){
-                int adjNode=adjs[0];
-                int weight=adjs[1];
+           
+            if (currTime > dist[node]) continue;
 
-                // relaxation
-                if(dist[node]+weight<dist[adjNode]){
-                    dist[adjNode]=dist[node]+weight;
-                    pq.push({adjNode,dist[adjNode]});
+            for (auto neighbor : adj[node]) {
+                int adjNode = neighbor.first;
+                int weight = neighbor.second;
+
+                if (dist[node] + weight < dist[adjNode]) {
+                    dist[adjNode] = dist[node] + weight;
+                    pq.push({dist[adjNode], adjNode}); 
                 }
-
             }
-
         }
 
-        int maxDist= *max_element(dist.begin()+1, dist.end());
-        return maxDist==INT_MAX ? -1 : maxDist;
-        
+       
+        int maxDist = *max_element(dist.begin() + 1, dist.end());
+        return maxDist == INT_MAX ? -1 : maxDist;
     }
 };
