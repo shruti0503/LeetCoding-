a0246a6d59df5db3ee0874c28b482286a15a5d0d
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-12-24 22:57:27 +0530

    Time: 0 ms (100%), Space: 9.2 MB (15.07%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -10,24 +10,24 @@ public:
         
         for (int i = 1; i < n; i++) {
             if (nums[i] == end + 1) {
-                end = nums[i];  // Expand the current range
+                end = nums[i];  // expand  current range
             } else {
                 if (start == end) {
-                    result.push_back(to_string(start));  // Single number, no range
+                    result.push_back(to_string(start));  
                 } else {
-                    result.push_back(to_string(start) + "->" + to_string(end));  // Add the range
+                    result.push_back(to_string(start) + "->" + to_string(end));  
                 }
                 
-                start = nums[i];  // Start a new range
+                start = nums[i];  // sttart  new range
                 end = nums[i];
             }
         }
         
-        // Handle the last range
+
         if (start == end) {
-            result.push_back(to_string(start));  // Single number, no range
+            result.push_back(to_string(start));  // single numbr, no range
         } else {
-            result.push_back(to_string(start) + "->" + to_string(end));  // Add the range
+            result.push_back(to_string(start) + "->" + to_string(end));  // add  range
         }
         
         return result;
