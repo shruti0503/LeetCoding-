e0260338deab986c1a1c676b9beae0371397e36c
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-12-31 03:32:20 +0530

    Time: 10 ms (33.72%), Space: 15.6 MB (46%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-int mod=1e9+7;
-int solve(int targetLength,int one,int zero,vector<int> &dp){
-    if(targetLength==0) return 1;
-    if(targetLength<0) return 0;
-    if(dp[targetLength]!=-1) return dp[targetLength];
-    long long sum;
-    sum=solve(targetLength-one,one,zero,dp)+solve(targetLength-zero,one,zero,dp);
-    return dp[targetLength]=sum%mod;
-}
+  int mod=1e9+7;
+   int func(int length, int one, int zero, vector<int>& dp){
+    if(length==0) return 1;
+    if(length<0) return 0;
+
+    if(dp[length]!=-1){
+        return dp[length];
+    }
+
+    int takeOne=func(length-one, one, zero, dp);
+    int takeZero=func(length-zero, one , zero, dp);
+
+    return dp[length]=(takeOne+takeZero)%mod;
+
+   }
     int countGoodStrings(int low, int high, int zero, int one) {
-        int ans=0; //storing the sum
-        vector<int> dp(high+1,-1); //to store the computations
+        int ans=0; 
+        vector<int> dp(high+1,-1); 
         for(int i=low;i<=high;i++){
-            ans=((ans%mod)+solve(i,one,zero,dp))%mod;
+            ans=((ans%mod)+func(i,one,zero,dp))%mod;
         }
         return ans;
     }
