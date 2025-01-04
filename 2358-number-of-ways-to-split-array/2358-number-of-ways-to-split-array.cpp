ea228d90fcfa36b9182afabf018805ca812f6464
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-01-05 02:01:34 +0530

    Time: 0 ms (100%), Space: 89.5 MB (38.16%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
-    long long totalSum = 0; // To hold the total sum of the array
+    long long totalSum = 0;
     for (int num : nums) {
         totalSum += num;
     }
 
-    long long prefixSum = 0; // To track the prefix sum
-    int count = 0;           // To count the valid splits
+    long long prefixSum = 0; 
+    int count = 0;          
 
-    // Iterate through the array to check valid splits
-    for (int i = 0; i < n - 1; i++) { // We stop at n-1 because the right part must be non-empty
+   
+    for (int i = 0; i < n - 1; i++) {
         prefixSum += nums[i];
         long long rightSum = totalSum - prefixSum;
         if (prefixSum >= rightSum) {
