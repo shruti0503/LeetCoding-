45f8f6389a3c59ac1983f30b7547bea29f3c53ed
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-01-18 21:11:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,10 +6,27 @@ public:
         dp[n]=func(dp, n-1)+func(dp, n-2);
         return dp[n];
     }
+    int tab(int n){
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(i==1){
+                dp[i]=dp[i-1];
+            }
+            else{
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+
+        }
+        return dp[n];
+
+
+    }
     int climbStairs(int n) {
         if(n==1) return 1;
         vector<int>dp(n+1, 0);
-        func(dp,n);
-        return dp[n];
+        return tab(n);
+        //return dp[n];
     }
 };
