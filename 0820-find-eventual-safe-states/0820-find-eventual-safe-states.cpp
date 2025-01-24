daedc423ac53144509cd4965e711fc0aee413634
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-01-25 00:01:47 +0530

    Time: 0 ms (100%), Space: 52.9 MB (75.14%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, int node, vector<int>& pathVis, vector<int>& vis, vector<int>& safeNode){
-         vis[node]=1;
-          pathVis[node] = 1; 
 
-         for(auto adj:graph[node]){
+    bool dfs(vector<vector<int>>& graph, int node, vector<int>& pathVis, vector<int>& vis, vector<int>& safeNode){
+        vis[node]=1;
+        pathVis[node]=1;
+
+        for(auto adj:graph[node]){
             if(!vis[adj]){
-               if( dfs(graph, adj, pathVis,vis , safeNode)){
-                 safeNode[node]=-1; //The purpose of safeNodes[node] = -1 is to indicate that node itself is unsafe because it leads to a cycle either directly or indirectly through one of its adjacent nodes (adj). Since node is calling the DFS on adj, any cycle detected in adj means that node is also part of an unsafe path.
-                 return true;
-               } 
+                if(dfs(graph, adj,pathVis, vis, safeNode)){
+                    safeNode[node]=-1;
+                    return true;
+                }
             }
             else if(pathVis[adj]){
                 safeNode[node]=-1;
                 return true;
             }
-         }
+        }
 
-         pathVis[node]=0;
-         safeNode[node]=1;
-         return false;
+        pathVis[node]=0;
+        safeNode[node]=1;
+        return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>vis(n,0);
         vector<int>pathVis(n,0);
-        vector<int> safeNodes(n,0);
+        vector<int>safeNodes(n,0);
 
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(graph, i, pathVis, vis,safeNodes);
+                dfs(graph, i,pathVis, vis,safeNodes);
             }
         }
-
         vector<int>ans;
         for(int i=0;i<n;i++){
             if(safeNodes[i]==1){
@@ -40,6 +40,5 @@ public:
             }
         }
         return ans;
-        
     }
 };
