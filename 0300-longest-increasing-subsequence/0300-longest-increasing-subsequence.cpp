7f0c2c94b94570ffd1dcca17afeea22ad7045ed3
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-03-05 23:31:36 +0530

    Time: 572 ms (12.11%), Space: 296.4 MB (20.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,64 +1,20 @@
 class Solution {
 public:
-    int func(vector<int>& nums, int ind, int prev, int n, vector<vector<int>>& dp) {
-        // Base case: If we've considered all elements
-        if (ind == n) return 0;
-        
-        // Check if the result is already computed
+     int func(vector<int>& nums, int ind, int prev , int n , vector<vector<int>>& dp){
+        if(ind==n) return 0;
         if (dp[ind][prev + 1] != -1) return dp[ind][prev + 1];
-        
-        // Not taking the current element
-        int notTake = func(nums, ind + 1, prev, n, dp);
-        
-        // Taking the current element (if it forms an increasing subsequence)
-        int take = 0;
-        if (prev == -1 || nums[ind] > nums[prev]) {
-            take = 1 + func(nums, ind + 1, ind, n, dp);
+        int notTake=func(nums, ind+1, prev, n, dp);
+        int take=0;
+        if(prev==-1 || nums[ind]>nums[prev]){
+            take=1+func(nums, ind+1, ind, n, dp);
+
         }
-        
-        // Store the result in dp array
-        return dp[ind][prev + 1] = max(take, notTake);
-    }
-
-   int tab(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prev = ind - 1; prev >= -1; prev--) {
-                int notTake = dp[ind + 1][prev + 1]; // -1 to 0
-                int take = 0;
-                if (prev == -1 || nums[ind] > nums[prev]) {
-                    take = 1 + dp[ind + 1][ind + 1]; // prev=-1 ind=5
-                }
-                dp[ind][prev + 1] = max(take, notTake);
-            }
-        }
-        return dp[0][-1 + 1];
-    }
-    int spaceOpt(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> curr(n + 1, 0), next(n + 1, 0);
-
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prev = ind - 1; prev >= -1; prev--) {
-                int notTake = next[prev + 1];
-                int take = 0;
-                if (prev == -1 || nums[ind] > nums[prev]) {
-                    take = 1 + next[ind + 1];
-                }
-                curr[prev + 1] = max(take, notTake);
-            }
-            next = curr;
-        }
-        return next[-1 + 1];
-    }
-
+        return dp[ind][prev + 1] =  max(take, notTake);
+     }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        //return func(nums, 0, -1, n, dp);
-       // return tab(nums);
-       return  spaceOpt(nums);
+        // 10 , 9. 
+        int n=nums.size();
+        vector<vector<int>>dp(n, vector<int>(n+1, -1));
+        return func(nums, 0, -1 , n, dp);
     }
 };
