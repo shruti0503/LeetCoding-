b1e651e3183908f303610a3391e9a6aef397737e
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-03-14 22:23:35 +0530

    Time: 0 ms (100%), Space: 18.1 MB (76.82%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
+     void dfs(vector<vector<int>>& image, vector<vector<int>>& ans, int sr, int sc, int color, int oldcolor){
+        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || ans[sr][sc]==color || image[sr][sc]!=oldcolor){
+            return;
+        }
+        ans[sr][sc]=color;
 
-  void dfs(vector<vector<int>>& image, vector<vector<int>>& ans, int sr, int sc, int color, int oldColor) {
-     if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != oldColor || ans[sr][sc] == color) {
-           return;
-      }
-
-
-     ans[sr][sc]=color;
-     dfs(image, ans, sr+1, sc, color,oldColor);
-     dfs(image, ans, sr-1, sc, color, oldColor);
-     dfs(image, ans, sr, sc+1, color, oldColor);
-     dfs(image, ans, sr, sc-1, color,oldColor);
-   }
+        dfs(image, ans, sr+1, sc, color, oldcolor);
+        dfs(image, ans, sr-1, sc, color, oldcolor);
+        dfs(image, ans, sr, sc+1, color, oldcolor);
+        dfs(image, ans, sr, sc-1, color, oldcolor);
+     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // m n 
-        // pixel value of the image.
-        //image[sr][sc]
-        // color -> fill neighbouring cells -> same color
-        // if they match the same color as the color[sr][sc]
-
-        // image[sr][sc]=1  -> color =2 
-        // if  adjcent cells are also the color 1 then color those too with the 2
-
-        int oldColor=image[sr][sc];
-        if(oldColor!=color){ // they shouldn't be same 
+        vector<vector<int>>ans;
+        int oldcolor=image[sr][sc];
+        if(oldcolor!=color){
             vector<vector<int>>ans=image;
-            dfs(image, ans, sr, sc, color, oldColor);
+            dfs(image, ans,sr,sc, color, oldcolor);
             return ans;
         }
         return image;
-
+        
     }
 };
