b62cccc796e14e0c274433f1f78c28e18e4b47fa
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-03-15 14:53:37 +0530

    Time: 0 ms (100%), Space: 13.8 MB (97%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
     void dfs(vector<vector<char>>& board, int i, int j, int n, int m) {
+
         if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] != 'O') return;
-
-        board[i][j] = 'T';  // Temporarily mark as safe
-        dfs(board, i + 1, j, n, m);
-        dfs(board, i - 1, j, n, m);
-        dfs(board, i, j + 1, n, m);
-        dfs(board, i, j - 1, n, m);
+        board[i][j]='T';
+        dfs(board, i-1, j, n, m);
+        dfs(board,i+1, j, n, m);
+        dfs(board,i,j+1, n, m);
+        dfs(board,i,j-1, n, m);
     }
-
     void solve(vector<vector<char>>& board) {
-        int n = board.size();
-        if (n == 0) return;
-        int m = board[0].size();
+        int n=board.size();
+        if(n==0) return ;
+        int m=board[0].size();
 
-        // Step 1: Mark all 'O's on the border and connected to border as safe
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O') dfs(board, i, 0, n, m);
-            if (board[i][m - 1] == 'O') dfs(board, i, m - 1, n, m);
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O') dfs(board,i,0 , n,m);
+            if(board[i][m-1]=='O') dfs(board,i,m-1,n,m);
+
         }
-        for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O') dfs(board, 0, j, n, m);
-            if (board[n - 1][j] == 'O') dfs(board, n - 1, j, n, m);
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O') dfs(board,0,j, n,m);
+            if(board[n-1][j]=='O') dfs(board,n-1,j,n,m);
         }
 
-        // Step 2: Flip all remaining 'O's to 'X'
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O') {
-                    board[i][j] = 'X';
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
 
-        // Step 3: Restore the safe 'O's back to 'O'
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'T') {
-                    board[i][j] = 'O';
-                }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='T') board[i][j]='O';
             }
         }
+
+       // return board;
+
+
+        
     }
 };
