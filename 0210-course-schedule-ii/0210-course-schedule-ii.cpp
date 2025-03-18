c044cb90088c71c743945a398790002baf093e45
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-03-18 23:26:51 +0530

    Time: 0 ms (100%), Space: 18.7 MB (33.98%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,42 +1,38 @@
-
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
         vector<int>indeg(numCourses,0);
-        vector<vector<int>>adjList(numCourses);
-        for(auto courses:prerequisites){
-            indeg[courses[0]]++;
-            adjList[courses[1]].push_back(courses[0]);
+        for(auto course:prerequisites){
+            adj[course[1]].push_back(course[0]);
+            indeg[course[0]]++;
         }
         queue<int>pq;
-        for(int i=0;i<indeg.size();i++){
-           if(indeg[i]==0){
-             pq.push(i); // push the node
-           }
-        }
-        //vector<pair<int,int>>  del={{0,1},{1,0},{-1,0}, {0,-1}};
         vector<int>topo;
+
+        for(int i=0;i<indeg.size();i++){
+            if(indeg[i]==0){
+                pq.push(i);
+            }
+        }
+
         while(!pq.empty()){
-            int node=pq.front();
+            int course=pq.front();
             pq.pop();
-            topo.push_back(node);
-            for(auto adj:adjList[node]){
-                indeg[adj]--;
-                if(indeg[adj]==0) {
-                    pq.push(adj);
+            topo.push_back(course);
+            for(auto adjNode:adj[course]){
+                indeg[adjNode]--;
+                if(indeg[adjNode]==0){
+                    pq.push(adjNode);
                 }
             }
         }
-       if (topo.size() == numCourses) {
+
+        if(topo.size()==numCourses){
             return topo;
         }
+
         return {};
-
-      
-
-       
-
-
+        
     }
 };
-
