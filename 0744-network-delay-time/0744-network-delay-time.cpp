1f4793ba0b3bef35d1d3229c2927766db79330f5
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-03-20 23:14:17 +0530

    Time: 85 ms (59.98%), Space: 48 MB (25.24%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n + 1);
-        for (auto it : times) {
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(auto it:times){
             adj[it[0]].push_back({it[1], it[2]});
         }
+         //min heap
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
 
-      
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, k}); // (time, node)
-
-        vector<int> dist(n + 1, INT_MAX); 
-        dist[k] = 0;
-
-        while (!pq.empty()) {
-            auto curr = pq.top();
+        pq.push({0,k});
+        vector<int>dist(n+1, INT_MAX);
+        dist[k]=0;
+        while(!pq.empty()){
+            auto curr=pq.top();
             pq.pop();
 
-            int currTime = curr.first;
-            int node = curr.second;
+            int currTime=curr.first;
+            int node=curr.second;
 
-           
-            if (currTime > dist[node]) continue;
+            if(currTime>dist[node]) continue;
+            for(auto neighbor:adj[node]){
+                int adjNode=neighbor.first;
+                int weight=neighbor.second;
 
-            for (auto neighbor : adj[node]) {
-                int adjNode = neighbor.first;
-                int weight = neighbor.second;
-
-                if (dist[node] + weight < dist[adjNode]) {
-                    dist[adjNode] = dist[node] + weight;
-                    pq.push({dist[adjNode], adjNode}); 
+                if(dist[node]+weight<dist[adjNode]){
+                     dist[adjNode]=dist[node]+weight;
+                     pq.push({dist[adjNode], adjNode});
                 }
+
             }
         }
 
-       
-        int maxDist = *max_element(dist.begin() + 1, dist.end());
-        return maxDist == INT_MAX ? -1 : maxDist;
+        int maxDist=*max_element(dist.begin()+1, dist.end());
+        return maxDist==INT_MAX ? -1 : maxDist;
+
+        
     }
 };
