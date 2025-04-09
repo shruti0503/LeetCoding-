50b6a88d3814d9a992be82397255509102cf4202
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-04-10 00:21:52 +0530

    Time: 1018 ms (16.01%), Space: 98.3 MB (5.06%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,30 +1,22 @@
 class ProductOfNumbers {
 public:
-    //stack<int>st;
     vector<int>arr;
+    int prod=1;
 
     ProductOfNumbers() {
         
     }
     
     void add(int num) {
-        //st.push(num);
         arr.push_back(num);
+    
     }
     
     int getProduct(int k) {
         int num=k;
         int prod=1;
-        //stack<int>s=st;
-        // while(i>=1 and s.size()){
-        //     int num=s.top();
-        //     s.pop();
-        //     prod*=num;
-        //     i--;
-        // }
         int n=arr.size()-1;
         for(int i=n;i>=0 and num>=1 ;i--){
-           // cout<<"arr"<<arr[i]<<endl;
             prod*=arr[i];
             num--;
         }
