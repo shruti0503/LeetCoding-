4bbc56adcf031b72c459c9567355876e128c8460
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-07-08 01:10:02 +0530

    Time: 3 ms (42.79%), Space: 19.3 MB (92.64%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -3,24 +3,27 @@ public:
 
     void func(int node, vector<vector<int>>& isConnected, vector<int>& vis){
         vis[node]=1;
-        for(int i=0;i<isConnected.size();i++){
-            if(isConnected[node][i] and !vis[i]){
+        for(int i=0;i<isConnected[node].size();i++){
+            if(!vis[i] and isConnected[node][i]){
                 func(i, isConnected, vis);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
+        // 1 , 1 ,0 
+    
         int n=isConnected.size();
         int m=isConnected[0].size();
+        vector<vector<int>>adj;
         int cnt=0;
         vector<int>vis(n,0);
-        for(int i=0;i<n;i++){
-            if(vis[i]!=1){
+        for(int i=0;i<n;i++){ // nodes : 0 , 1 , 2 
+            if(!vis[i]){
                 func(i,isConnected,vis);
                 cnt++;
             }
         }
-
         return cnt;
+        
     }
 };
