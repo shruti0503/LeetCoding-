8eea0f939431c487d98fe674cdb6d429750d5428
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-07-09 00:12:32 +0530

    Time: 0 ms (100%), Space: 17.9 MB (90.46%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-     void dfs(vector<vector<int>>& image, vector<vector<int>>& ans, int sr, int sc, int color, int oldcolor){
-        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || ans[sr][sc]==color || image[sr][sc]!=oldcolor){
+    void dfs(vector<vector<int>>& image, vector<vector<int>>& ans, int sr, int sc, int color, int oldColor) {
+        // Check if we're out of bounds or if the cell is already the color we want
+        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size()
+         || ans[sr][sc] == color || image[sr][sc] != oldColor) {
             return;
         }
-        ans[sr][sc]=color;
 
-        dfs(image, ans, sr+1, sc, color, oldcolor);
-        dfs(image, ans, sr-1, sc, color, oldcolor);
-        dfs(image, ans, sr, sc+1, color, oldcolor);
-        dfs(image, ans, sr, sc-1, color, oldcolor);
-     }
+        // Set the color
+        ans[sr][sc] = color;
+
+        // Move in all four directions
+        dfs(image, ans, sr + 1, sc, color, oldColor);
+        dfs(image, ans, sr - 1, sc, color, oldColor);
+        dfs(image, ans, sr, sc + 1, color, oldColor);
+        dfs(image, ans, sr, sc - 1, color, oldColor);
+        
+    }
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>>ans;
-        int oldcolor=image[sr][sc];
-        if(oldcolor!=color){
-            vector<vector<int>>ans=image;
-            dfs(image, ans,sr,sc, color, oldcolor);
+        int oldColor = image[sr][sc];
+        // Only start DFS if the color is different
+        if (oldColor != color) {
+            vector<vector<int>> ans = image;
+            dfs(image, ans, sr, sc, color, oldColor);
             return ans;
         }
         return image;
-        
     }
 };
