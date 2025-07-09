6c6e2fe5baab3e68c1f3b351c544a4ffbc41767a
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-07-09 23:57:03 +0530

    Time: 22 ms (48.15%), Space: 34.8 MB (79.02%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>>ans(n, vector<int>(m,INT_MAX));
-
-
-        queue<pair<int,int>>pq;
-       // int n=pq.size()
-       for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
-                    pq.push({i,j});
-                    ans[i][j]=0;
-
-                } 
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+       int rows = matrix.size();
+        if (rows == 0)
+            return matrix;
+        int cols = matrix[0].size();
+        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX));
+        queue<pair<int, int>> q;
+        //start by adding all the zeros to the queue
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.push({ i, j }); //Put all 0s in the queue.
+                }
             }
-       }
+        }
+        //for tarcking in all th four direction
 
-       vector<vector<int>>dir={{0,1}, {1,0}, {-1,0},{0,-1}};
-
-        while(!pq.empty()){
-            auto pos=pq.front();
-            pq.pop();
-            int row=pos.first;
-            int col=pos.second;
-
-            for(auto d:dir){
-                int r=d[0]+row;
-                int c=d[1]+col;
-
-                if(r>=0 and r<n and c>=0 and c<m){
-                    if(ans[r][c]>ans[row][col]+1){
-                        ans[r][c]=ans[row][col]+1;
-                        pq.push({r,c});
+        int dir[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        while (!q.empty()) {
+            pair<int, int> curr = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int new_r = curr.first + dir[i][0], new_c = curr.second + dir[i][1];
+                if (new_r >= 0 && new_c >= 0 && new_r < rows && new_c < cols) {
+                    if (dist[new_r][new_c] > dist[curr.first][curr.second] + 1) {
+                        dist[new_r][new_c] = dist[curr.first][curr.second] + 1;
+                        q.push({ new_r, new_c });
                     }
                 }
-
             }
-
-
         }
+        return dist;
 
-        return ans;
+    
+        
     }
 };
