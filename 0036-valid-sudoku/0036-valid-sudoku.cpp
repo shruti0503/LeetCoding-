d99a2b75ec88b1c5dabe6c31a0d83272b94c4d89
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-08-31 01:39:28 +0530

    Time: 19 ms (6.31%), Space: 22.7 MB (70.32%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,27 +1,60 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-          int a[9][9]={0},b[9][9]={0},c[9][9]={0};
-        for(int i=0;i<board.size();++i){
-            for(int j=0;j<board.size();++j){
-                if(board[i][j]!='.'){
-                    int num=board[i][j]-'0'-1;
-//  The character at the (i, j) position is first converted to its corresponding ASCII code, and then subtracting the ASCII code of '0' gives us the numerical value of the digit. For example, the ASCII code of '0' is 48, and the ASCII code of '5' is 53. Thus, '5' - '0' gives 5, the numerical value of the digit.
+  bool isValidSudoku(vector<vector<char>>& board) {
+    // check each row
+    for (int i = 0; i < 9; i++) {
+        vector<bool> used(9, false);
+        for (int j = 0; j < 9; j++) {
+            char val = board[i][j];
+            if (val == '.') {
+                continue;
+            }
+            int index = val - '1';
+            if (used[index]) {
+                return false;
+            }
+            used[index] = true;
+        }
+    }
 
-// However, since the valid range of numbers in the sudoku board is from 1 to 9, we subtract 1 from num to convert it to a valid index in the range of 0 to 8.
+    // check each column
+    for (int j = 0; j < 9; j++) {
+        vector<bool> used(9, false);
+        for (int i = 0; i < 9; i++) {
+            char val = board[i][j];
+            if (val == '.') {
+                continue;
+            }
+            int index = val - '1';
+            if (used[index]) {
+                return false;
+            }
+            used[index] = true;
+        }
+    }
 
-// For example, if the character at (i, j) position is '5', then num will be assigned a value of 4 (since 5 - '0' = 5, and 5 - 1 = 4).                   
-                    int k= i / 3 * 3 + j / 3;
-                    if(a[i][num]||b[j][num]||c[k][num]){
+    // check each 3x3 sub-box
+    for (int box_row = 0; box_row < 3; box_row++) {
+        for (int box_col = 0; box_col < 3; box_col++) {
+            vector<bool> used(9, false);
+            for (int i = box_row * 3; i < box_row * 3 + 3; i++) {
+                for (int j = box_col * 3; j < box_col * 3 + 3; j++) {
+                    char val = board[i][j];
+                    if (val == '.') {
+                        continue;
+                    }
+                    int index = val - '1';
+                    if (used[index]) {
                         return false;
                     }
-                        a[i][num]=b[j][num]=c[k][num]=1;
+                    used[index] = true;
                 }
-
-            
             }
-
         }
-        return true;
     }
+
+    return true;
+}
+
+
 };
