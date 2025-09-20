9f2af3619c82bfdbb89437c54c58433a4dc9f8c3
Shruti <94033084+shruti0503@users.noreply.github.com>
2025-09-21 00:08:22 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,39 +1,13 @@
 class Solution {
-    //brute force app would be to take a sep array and push forst all the non zero elements and 
-    //then travserse again and push all the zeros
-    //sc o(n) tco(n)
 public:
-// Initial vector: {0, 1, 0, 3, 12}
-// lastNonZeroFoundAt = 0, cur = 0
-
-// // First iteration: cur = 0
-// nums[0] = 0, which is zero, so we don't do anything
-
-// // Second iteration: cur = 1
-// nums[1] = 1, which is not zero, so we swap it with nums[lastNonZeroFoundAt]
-// nums: {1, 0, 0, 3, 12}, lastNonZeroFoundAt = 1
-
-// // Third iteration: cur = 2
-// nums[2] = 0, which is zero, so we don't do anything
-
-// // Fourth iteration: cur = 3
-// nums[3] = 3, which is not zero, so we swap it with nums[lastNonZeroFoundAt]
-// nums: {1, 3, 0, 0, 12}, lastNonZeroFoundAt = 2
-
-// // Fifth iteration: cur = 4
-// nums[4] = 12, which is not zero, so we swap it with nums[lastNonZeroFoundAt]
-// nums: {1, 3, 12, 0, 0}, lastNonZeroFoundAt = 3
-
-// // Loop is done, all the zeroes are at the end of the vector
-// Final vector: {1, 3, 12, 0, 0}
-
-
     void moveZeroes(vector<int>& nums) {
-    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
-        if (nums[cur] != 0) {
-            swap(nums[lastNonZeroFoundAt], nums[cur]);
-            lastNonZeroFoundAt++;
+        int lastNonZero=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[lastNonZero]);
+                lastNonZero++;
+            }
         }
+        
     }
-}
 };
