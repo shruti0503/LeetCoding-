f4cda7edccc5cb109db7e8f43de9a8b328c4e565
Shruti <94033084+shruti0503@users.noreply.github.com>
2026-06-02 23:46:46 +0530

    Time: 0 ms (100%), Space: 19.3 MB (87%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,27 +11,28 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode();  // Dummy node to simplify the handling of the head of the merged list
-        ListNode* curr = dummy;  // Pointer to build the merged list
+        ListNode* dummy = new ListNode();
+        ListNode* curr=dummy;
         
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                curr->next = list1;  // Attach the smaller node to the merged list
-                list1 = list1->next;  // Move to the next node in list1
-            } else {
-                curr->next = list2;  // Attach the smaller node to the merged list
-                list2 = list2->next;  // Move to the next node in list2
+        while(list1!=nullptr and list2 != nullptr){
+            if(list1->val<=list2->val){
+                curr->next=list1;
+                list1=list1->next;
             }
-            curr = curr->next;  // Move the current pointer to the next node
+            else{
+                curr->next=list2;
+                list2=list2->next;
+            }
+            curr=curr->next;
         }
-        
-        // Attach the remaining part of the list that is not yet exhausted
-        if (list1 != nullptr) {
-            curr->next = list1;
-        } else {
-            curr->next = list2;
+
+        if(list1 != nullptr){
+            curr->next=list1;
         }
+        else{
+            curr->next=list2;
+        }
+        return dummy->next;
         
-        return dummy->next;  // Return the merged list starting from the first node
     }
 };
