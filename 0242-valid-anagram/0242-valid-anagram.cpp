7b68a5a442d5a69b1f9b3b77e1cd0a125846d548
Shruti <94033084+shruti0503@users.noreply.github.com>
2026-06-07 03:31:28 +0530

    Time: 10 ms (6.86%), Space: 9.6 MB (75.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int>mpp;
-        for(auto i:s){
-            mpp[i]++;
-        }
-        for(auto i:t){
-            //element is found bedfore end of map
-            if(mpp.find(i)!=mpp.end()){
-                mpp[i]--;
-            }
-            else{
-                return false;
-            }
-        }
-
-        for(auto i:mpp){
-            if(i.second!=0) return false;
-        }
-
-        return true;
-
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+        
     }
 };
